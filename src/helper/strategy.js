254c622641c4ed2dbc90890a4dc5c4c70a39bfcb
Leonardo Pacheco <leonardo.pacheco@b8one.com>
2021-04-03 19:56:38 -0300

    feat: change commands

--- src/helper/strategy.js
+++ src/helper/strategy.js
@@ -1,24 +1,17 @@
 const mostrarProps = require("./helper");
 
-const rand_imgs_guigui = [
-  './src/assets/images/guigui/guigui1.jpg',
-  './src/assets/images/guigui/guigui2.jpg',
-  './src/assets/images/guigui/guigui3.jpg',
-  './src/assets/images/guigui/guigui4.jpg',
-  './src/assets/images/guigui/guigui5.jpg',
-]
-
 const helper = {
   "!calcinha": "Christian de calzinha meeeooo",
   "!bory": "EU VOCÊ E O BORY, mano!!!!!!!!",
   "!transadinha": "SÓ UMA MANO, SÓ UMA TRANSADINHA",
-  "!bom dia": "Bom dia Pedrão",
+  "bom dia": "Bom dia Pedrão",
+  "boa tarde": "Boa tarde Pedrão",
+  "bom noite": "Boa noite Pedrão",
   "!xandao": "Xandão é o ser mais energizado do mundo meeeooo",
   "!kim": "Famoso Kina betch",
   "!diogo": "Famoso diogo diogando",
   "!patonana": "Famoso sakin sakin sakin",
   "!pacheco": "Famoso busca longe",
-  // "!guigui": ("Olha o guegue", { files: [rand_imgs_guigui[Math.floor(Math.random() * rand_imgs_guigui.length)]] }),
   "!dodoidacabeca":('ola o dodoi da cabeça', { files: ['./src/assets/images/dodoi-da-cabeca.jpg'] }),
   "!gostosa": ('Olha a gostosa', { files: ['./src/assets/images/gostosa.jpg'] }),
 };
